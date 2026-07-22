edeb0b4bcaa0980719e2102fb9cddd6508229d81
Shafin Ashrafin <mdshafinashrafin753@gmail.com>
2026-07-22 10:20:41 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-
+export default {
+  async fetch(request: Request): Promise<Response> {
+    return new Response("Messenger AI Bot is running!", {
+      status: 200,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  },
+};
